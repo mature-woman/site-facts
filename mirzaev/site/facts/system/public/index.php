52de8d9360d0fb309c5ad57335c8285487b68786
mirzaev <arsen@mirzaev.sexy>
2023-07-09 04:35:55 +0700

    porno mama

---
+++ mirzaev/site/facts/system/public/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+
+<html lang="ru">
+
+<head>
+	<title>Important Facts</title>
+	<meta charset="utf-8">
+<meta name="viewport" content="width=800px" />
+	<meta name="HandheldFriendly" content="true" />
+	<meta name="MobileOptimized" content="width" />
+	<meta content='yes' name='apple-mobile-web-app-capable' />
+	<meta name="description" content="Thoughts that will make you think" />
+	<meta name="keywords" content="facts, important, things, society" />
+	<meta name="author" content="MIRZAEV" />
+	<meta name="revised" content="08-07-2023" />
+	<meta property="og:title" content="Important Facts" />
+	<meta property="og:type" content="article" />
+	<meta property="article:published_time" content="2023-07-08" />
+	<meta property="article:author" content="MIRZAEV" />
+	<meta property="article:section" content="article" />
+	<meta property="article:tag" content="facts, important, things, society" />
+	<meta property="og:url" content="https://facts.mirzaev.sexy/" />
+	<meta property="og:image" content="https://facts.mirzaev.sexy/images/cover.jpg" />
+	<meta property="og:description" content="Thoughts that will make you think" />
+	<meta property="og:site_name" content="mirzaev.sexy" />
+	<meta property="twitter:card" content="summary">
+	<meta property="twitter:creator" content="@mirzaev_sexy">
+	<meta property="twitter:site" content="@mirzaev_sexy">
+	<meta property="twitter:url" content="https://facts.mirzaev.sexy/">
+	<meta property="twitter:title" content="Important Facts">
+	<meta property="twitter:description" content="Thoughts that will make you think">
+	<meta property="twitter:image" content="https://facts.mirzaev.sexy/images/cover.jpg">
+	<link type="text/css" rel="stylesheet" href="/css/main.css" />
+	<link type="text/css" rel="stylesheet" href="/css/fonts/fira.css" />
+</head>
+
+<body>
+	<article id="facts">
+		<section>
+			<img src="/images/nigger.webp" alt="NIGGER" class="unselectable" ondragstart="return false">
+			<h2>NIGGER</h2>
+			<p>Black skin human</p>
+		</section>
+		<section>
+			<img src="/images/faggot.webp" alt="FAGGOT" class="unselectable" ondragstart="return false">
+			<h2>FAGGOT</h2>
+			<p>Stretched human</p>
+		</section>
+		<section>
+			<img src="/images/woman.webp" alt="WOMAN" class="unselectable" ondragstart="return false">
+			<h2>WOMAN</h2>
+			<p>Female human</p>
+		</section>
+	</article>
+	<section id="conclusion">
+		<p>Thing about it</p>
+	</section>
+</body>
+
+</html>
